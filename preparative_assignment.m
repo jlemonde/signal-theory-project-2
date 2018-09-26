7c9a05dbacdcb4177c1dcab86cb2570cdd6a3723
jlemonde <lemondejoj.o@gmail.com>
2018-09-26 17:29:33 +0200

    commencé

--- preparative_assignment.m
+++ preparative_assignment.m
@@ -1,13 +1,19 @@
 clear;
 
+%%%%%%%%%%% DÉCODAGE ET ENCODAGE, PRÉSENTATION DES DEUX FONCTIONS
+%%%%%%%%%%% RÉCIPROQUES.
 % pic = imread('kth.jpg');
 % [key, cPic] = encoder(pic);
 % dPic = decoder(key, cPic);
 % image(dPic);
 
+
+
+%%%%%%%%%%% ENCODAGE DE L'IMAGE
 pic = imread('kth.jpg');
 [key, cPic] = encoder(pic);
 
+%%%%%%%%%%% DISTORTION DU SIGNAL : CONVOLUTION
 h = [1, 0.7, 0.7, 0];
 % rKey = zeros(size(key));
 % for k = 1:length(key)
@@ -17,7 +23,10 @@ h = [1, 0.7, 0.7, 0];
 %         end
 %     end
 % end
-rKey = filter(h, 1, key); % this does the same as the comment before
+rKey = filter(h, 1, key); % this line does the same as the comment before
+
+%%%%%%%%%%% À LA RÉCEPTION, ON APPLIQUE LA FONCTION SIGNE (ON VEUT QUE DES
+%%%%%%%%%%% 1 ET DES -1)
 pKey = zeros(size(rKey));
 for k = 1:length(pKey)
     pKey(k) = sign(rKey(k));
@@ -26,5 +35,11 @@ for k = 1:length(pKey)
     end
 end
 
+%%%%%%%%%%% DÉCODAGE DE L'IMAGE ET AFFICHAGE DE CELLE-CI
 dPic = decoder(pKey, cPic);
 image(dPic);
+
+
+
+
+
